c54de17033609060cedd652648a3d80b7bb91103
kaizen857 <3064561709@qq.com>
2025-03-10 11:11:39 +0800

    实验4

---
+++ LoginClient/src/com/yychat/view/FriendList.java
@@ -0,0 +1,48 @@
+package com.yychat.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FriendList extends JFrame {
+    public FriendList(){
+        JPanel friendPanel = new JPanel(new BorderLayout());
+        JButton friendButton1 = new JButton("我的好友");
+        JButton strangerButton1 = new JButton("陌生人");
+
+        JButton blackListButton1 = new JButton("黑名单");
+        friendPanel.add(friendButton1, "North");
+
+        JPanel strangerBlackPanel = new JPanel(new GridLayout(2,1));
+        strangerBlackPanel.add(strangerButton1);
+        strangerBlackPanel.add(blackListButton1);
+        friendPanel.add(strangerBlackPanel, "South");
+
+        JPanel strangerPanel = new JPanel(new BorderLayout());
+        JButton friendButton2 = new JButton("我的好友");
+        JButton strangerButton2 = new JButton("陌生人");
+        JPanel friendStrangerPanel = new JPanel(new GridLayout(2,1));
+        friendStrangerPanel.add(friendButton2);
+        friendStrangerPanel.add(strangerButton2);
+        strangerPanel.add(friendStrangerPanel, "North");
+
+        JButton blackListButton2 = new JButton("黑名单");
+        strangerPanel.add(blackListButton2, "South");
+
+        CardLayout cardLayout = new CardLayout();
+        this.setLayout(cardLayout);
+        this.add(friendPanel, "Card1");
+        this.add(strangerPanel, "Card2");
+        cardLayout.show(this.getContentPane(),"Card1");
+        strangerButton1.addActionListener(e -> {cardLayout.show(this.getContentPane(),"Card2");});
+        friendButton2.addActionListener(e -> {cardLayout.show(this.getContentPane(),"Card1");});
+        this.setIconImage(new ImageIcon("./res/duck2.gif").getImage());
+        this.setTitle("好友列表");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(800, 600,350,250);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FriendList friendList = new FriendList();
+    }
+}
