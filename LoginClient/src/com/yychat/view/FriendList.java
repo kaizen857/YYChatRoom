86ad2b1933642e0047ec3176eed6444c35e12c85
kaizen857 <3064561709@qq.com>
2025-03-14 19:15:12 +0800

    实验四（部分）

--- LoginClient/src/com/yychat/view/FriendList.java
+++ LoginClient/src/com/yychat/view/FriendList.java
@@ -10,12 +10,14 @@ public class FriendList extends JFrame {
         JButton strangerButton1 = new JButton("陌生人");
 
         JButton blackListButton1 = new JButton("黑名单");
-        friendPanel.add(friendButton1, "North");
+        friendPanel.add(friendButton1, BorderLayout.NORTH);
+
+
 
         JPanel strangerBlackPanel = new JPanel(new GridLayout(2,1));
         strangerBlackPanel.add(strangerButton1);
         strangerBlackPanel.add(blackListButton1);
-        friendPanel.add(strangerBlackPanel, "South");
+        friendPanel.add(strangerBlackPanel, BorderLayout.SOUTH);
 
         JPanel strangerPanel = new JPanel(new BorderLayout());
         JButton friendButton2 = new JButton("我的好友");
@@ -23,18 +25,20 @@ public class FriendList extends JFrame {
         JPanel friendStrangerPanel = new JPanel(new GridLayout(2,1));
         friendStrangerPanel.add(friendButton2);
         friendStrangerPanel.add(strangerButton2);
-        strangerPanel.add(friendStrangerPanel, "North");
+        strangerPanel.add(friendStrangerPanel, BorderLayout.NORTH);
 
         JButton blackListButton2 = new JButton("黑名单");
-        strangerPanel.add(blackListButton2, "South");
+        strangerPanel.add(blackListButton2, BorderLayout.SOUTH);
 
         CardLayout cardLayout = new CardLayout();
         this.setLayout(cardLayout);
         this.add(friendPanel, "Card1");
         this.add(strangerPanel, "Card2");
         cardLayout.show(this.getContentPane(),"Card1");
+
         strangerButton1.addActionListener(e -> {cardLayout.show(this.getContentPane(),"Card2");});
         friendButton2.addActionListener(e -> {cardLayout.show(this.getContentPane(),"Card1");});
+
         this.setIconImage(new ImageIcon("./res/duck2.gif").getImage());
         this.setTitle("好友列表");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
